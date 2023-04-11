4dd81963cfd3fd0780ab45104c677060c4cf3917
LeandroVidal56 <leandrovidal56@gmail.com>
2023-04-11 13:27:37 -0300

    background-image

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto'
 import { Loading } from '@components/Loading';
 import { THEME } from 'src/theme';
+import { SignIn } from '@screens/index';
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold})
   return (
@@ -14,7 +15,7 @@ export default function App() {
         translucent
         />
       {fontsLoaded ? 
-      <Text>Hello World!</Text>
+      <SignIn/>
       : 
       <Loading/>
     }
