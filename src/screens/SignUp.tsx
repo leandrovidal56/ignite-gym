4a280bd25a51d50eeb937f2df9e33e202c0f9fba
LeandroVidal56 <leandrovidal56@gmail.com>
2023-04-17 20:52:05 -0300

    finished-http-client

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,4 +1,4 @@
-import {VStack, Image, Text, Center, Heading, ScrollView } from 'native-base'
+import {VStack, Image, Text, Center, Heading, ScrollView, useToast } from 'native-base'
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png';
 import { Input } from '@components/Input';
@@ -7,6 +7,10 @@ import { useNavigation } from '@react-navigation/native';
 import { useForm, Controller} from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup'
 import * as yup from 'yup';
+import { api } from '@services/api';
+import axios from 'axios';
+import { Alert } from 'react-native';
+import { AppError } from '@utils/AppError';
 
 type FormDataProps = {
     name: string;
@@ -28,13 +32,27 @@ export function SignUp(){
         resolver: yupResolver(signUpSchema)
     });
     const navigation = useNavigation();
+    const toast = useToast()
     
 
     function handleGoBack(){
         navigation.goBack()
     }
-    function handleSignUp({ name, email, password, password_confirm }: FormDataProps) {
-        console.log(name, email, password, password_confirm, 'take data')
+    async function handleSignUp({ name, email, password }: FormDataProps) {
+        try{
+            const response = await api.post('/users', {name, email, password});
+            console.log(response.data)
+        }catch(error){
+          const isAppError = error instanceof AppError
+          const title = isAppError ? error.message : 'Não foi possível criar a conta. Tente novamente mais tarde.' 
+
+          toast.show({
+            title,
+            placement: 'top',
+            bgColor: 'red.500'
+          })
+
+        }
     }
     return (
         <ScrollView contentContainerStyle={{ flexGrow: 1}} showsVerticalScrollIndicator={false}>
---
+++ src/services/api.ts
@@ -0,0 +1,15 @@
+import { AppError } from '@utils/AppError';
+import axios from 'axios';
+
+const api = axios.create({
+    baseURL: 'http://192.168.15.9:3333'
+})
+api.interceptors.response.use(response => response, error => {
+    if(error.response && error.response.data){
+        return Promise.reject(new AppError(error.response.data.message))
+    }else {
+        return Promise.reject(error)
+    }
+})
+
+export {api}
